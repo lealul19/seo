d0943629d1f9f99303d97c111993dd1507d575a2
lealul19 <94169536+lealul19@users.noreply.github.com>
2026-03-04 12:53:11 +0100

    testen 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,24 @@
 <script>
     import background from '$lib/assets/background.jpg';
 </script>
+
+<svelte:head>
+    <title>Lea's Webshop - Electronics for all!</title>
+    <meta name= "description" content="We sell used electronics for a super cheap price!">
+    <meta property="og:title" content="Gesart's Webshop - Electronics for all" />
+    <meta property="og:image" content="{background}" />
+</svelte:head>
  
 <header class="h-[500px] bg-center bg-cover flex items-center justify-center"
     style="background-image: url({background})">
-    <h1 class="text-5xl text-white p-4">Electronic Devices</h1>
-</header>
- 
-<main>
+       <h1 class="text-5xl font-bold text-white">Electronic Devices</h1>
+    </header>
+    <main class="min-h-[300px]"> 
  
 </main>
  
-<footer>
- 
+<footer class="h-[300px] bg-blue-950 text-white flex items-center justify-center">
+    <p>Test-Page for SEO</p>
 </footer>
 
 
