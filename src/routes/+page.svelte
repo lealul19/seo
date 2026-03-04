feeb89d570f48547e1dc65e60878a7ca569493e5
lealul19 <94169536+lealul19@users.noreply.github.com>
2026-03-04 12:33:40 +0100

    seo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import background from '$lib/assets/background.jpg';
+</script>
+ 
+<header class="h-[500px] bg-center bg-cover flex items-center justify-center"
+    style="background-image: url({background})">
+    <h1 class="text-5xl text-white p-4">Electronic Devices</h1>
+</header>
+ 
+<main>
+ 
+</main>
+ 
+<footer>
+ 
+</footer>
+
+
+
